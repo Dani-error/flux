431d519a22b3bc29311fc13cc953f4df967d7952
Dani-error <zdani.error@gmail.com>
2025-07-11 13:25:03 +0100

    feat: publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,18 @@
+import com.vanniktech.maven.publish.MavenPublishBaseExtension
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.lang.Integer.parseInt
+
 plugins {
     kotlin("jvm") version "2.1.21"
     id("org.jetbrains.dokka") version "2.0.0"
     java
+    id("com.vanniktech.maven.publish") version "0.33.0"
 }
 
-group = "io.github.dani-error"
-version = "1.0.0-SNAPSHOT"
+defaultTasks("clean", "build")
+
+group = project.property("GROUP")!!.toString()
+version = project.property("VERSION_NAME")!!.toString()
 
 repositories {
     mavenCentral()
@@ -38,4 +45,66 @@ dokka {
 
 tasks.build {
     dependsOn(tasks.dokkaGenerate)
+}
+
+val javaVersion = project.property("JAVA_VERSION")!!.toString()
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.fromTarget(javaVersion))
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.release.set(parseInt(javaVersion))
+}
+
+tasks.withType<Jar> {
+    from(rootProject.file("LICENSE"))
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
+
+extensions.configure<MavenPublishBaseExtension> {
+    publishToMavenCentral()
+    signAllPublications()
+
+    publishing {
+        repositories {
+            maven {
+                name = "githubPackages"
+                url = uri("https://maven.pkg.github.com/Dani-error/flux")
+                credentials(PasswordCredentials::class)
+            }
+        }
+    }
+
+    pom {
+        name = "Flux"
+        description = project.property("DESCRIPTION")!!.toString()
+        inceptionYear = project.property("INCEPTION_YEAR")!!.toString()
+        url = project.property("PROJECT_URL")!!.toString()
+        licenses {
+            license {
+                name = project.property("LICENSE_NAME")!!.toString()
+                url = project.property("LICENSE_URL")!!.toString()
+            }
+        }
+        ciManagement {
+            system.set(project.property("CI_SYSTEM")!!.toString())
+            url.set(project.property("CI_URL")!!.toString())
+        }
+        developers {
+            developer {
+                id = project.property("DEVELOPER_ID")!!.toString()
+                name = project.property("DEVELOPER_NAME")!!.toString()
+                url = project.property("DEVELOPER_URL")!!.toString()
+            }
+        }
+        scm {
+            val repoUrl = project.property("PROJECT_URL")!!.toString()
+            url = repoUrl
+            connection = "scm:git:git://github.com/Dani-error/flux.git"
+            developerConnection = "scm:git:ssh://git@github.com/Dani-error/flux.git"
+        }
+    }
 }
